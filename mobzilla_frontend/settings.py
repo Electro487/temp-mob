baba89e0a9c855cf17e148bf3d6ccace383df073
Electro487 <sujaladhikari487@gmail.com>
2025-09-15 16:03:50 +0545

    first commitsssssss

--- mobzilla_frontend/settings.py
+++ mobzilla_frontend/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-8*!7zghz+tqrd5)z#+vt(1(xr_44uf*#@91eu5^e)uw%mx1&l0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["temp-mob.onrender.com", "localhost", "127.0.0.1"]
+
 
 
 # Application definition
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # add this line
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
